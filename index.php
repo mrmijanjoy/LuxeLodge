9aeb558b9804c59053439573a7b8b390279644a7
Mijanur Rahman Joy <mrmijanjoy@gmail.com>
2024-09-14 12:46:38 +0600

    Navbar initialized

--- index.php
+++ index.php
@@ -10,8 +10,52 @@
 
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<style>
+		body {
+			font-family: 'Poppins', sans-serif;
+		}
+		.h-font {
+			font-family: "Merienda", cursive;
+		}
+	</style>
+
 </head>
 <body>
+
+
+	<!-- navbar -->
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light px-lg-3 py-lg-2 shadow-sm sticky-top">
+	  <div class="container-fluid">
+	    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">LuxeLodge</a>
+	    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        <li class="nav-item">
+	          <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link me-2" href="#">Rooms</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link me-2" href="#">Facilities</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link me-2" href="#">Contact us</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">About</a>
+	        </li>
+	      </ul>
+	      <div class="d-flex" role="search">
+	        <button class="btn btn-outline-success shadow-none" type="submit">Search</button>
+	      </div>
+	    </div>
+	  </div>
+	</nav>
 
 
 
